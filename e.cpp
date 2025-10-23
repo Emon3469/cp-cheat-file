dec0828d8300defd9320eeb598b37e9409941ade
mdsahadathossainemon@gmail.com <mdsahadathossainemon@gmail.com>
2025-10-23 17:37:21 +0600

    first commit

---
+++ e.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N = 2e5 + 20;
+#define mod 1000000007
+map<int,int> dp;
+void file(){
+    #ifdef velox
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    #endif
+}
+void solve()
+{
+  
+}
+int main()
+{
+    //file();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+      solve();
+    }
+}
